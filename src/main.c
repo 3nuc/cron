fd48168f19d56dd5782ac404e91d81fb16a95428
Zaawansowane systemy operacyjne <zso@n17>
2018-03-28 18:49:35 +0200

    Add arguments and error handling

--- src/main.c
+++ src/main.c
@@ -1,13 +1,28 @@
 #include "taskfileparse.h"
 #include <stdio.h>
+#include <errno.h>
 
-int main() {
+int main(int argc, char * argv[]) {
 	
-	const char* pathToTaskfile = "taskfile.test"; //this needs to be read from arguments (eg ./minicron path)
+	if(argc > 3) { 
+		errno=E2BIG;
+		perror("Too many arguments (2 needed)");
+		return errno;
+	}
+
+	else if (argc<3) {
+		errno=; //no error code for too few arguments btw
+		fprintf(stderr, "Too few arguments (2 needed)");
+		return errno;
+	}
+
+	const char* pathToTaskfile = argv[1]; //this needs to be read from arguments (eg ./minicron path)
+	const char* pathToOutfile = argv[2];
+
 	struct TASKFILE_LINE *tasks = getTaskArray(pathToTaskfile);
-	const int numberTasks = sizeof(tasks)/sizeof(char*)+1;
+	const int numberOfTasks = sizeof(tasks)/sizeof(char*)+1;
 	
-	for(int i = 0; i<numberTasks; i++)
+	for(int i = 0; i<numberOfTasks; i++)
 		printTask(tasks[i]);
 	
 	
