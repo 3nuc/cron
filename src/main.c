3219719422770606d587372f5a9a48295083ba56
3nuc <atagisov@gmail.com>
2018-03-28 14:59:33 +0200

    Add main.c example for how to work on the "task API"

--- src/main.c
+++ src/main.c
@@ -3,10 +3,17 @@
 
 int main() {
 	
-	char *tasks = _getTaskfileContents("taskfile.test");
-//	printf("%s",tasks);
-	char **tasktable=_convertLineStringIntoLineArray(tasks);
-
-	printf("%s",tasktable[0]);
+	const char* pathToTaskfile = "taskfile.test"; //this needs to be read from arguments (eg ./minicron path)
+	struct TASKFILE_LINE *tasks = getTaskArray(pathToTaskfile);
+	const int numberTasks = sizeof(tasks)/sizeof(char*)+1;
+	
+	for(int i = 0; i<numberTasks; i++)
+		printTask(tasks[i]);
+	
+	
+	
+	
+	//memory cleanup
+	free(tasks);
 	return 0;
 }
