6747f7d63de32d16efd850f6760df2a5fd1339cf
artur <artur@t430.t430>
2018-04-10 09:26:49 -0500

    Add removeEdgeSpaces, fix some function i dont remember, add docs

--- src/main.c
+++ src/main.c
@@ -6,25 +6,39 @@ void handleCommand(char* commandString) {
 
 	if(stringContainsCharacter(commandString,'|')) { //if command is like 'program1 | program2'
 		int* numberOfPipedCommands=malloc(sizeof *numberOfPipedCommands);
-		char** commands = splitByPipe(commandString, numberOfPipedCommands);
+		char** commands = splitByCharacter(commandString, numberOfPipedCommands, '|');
 			
 		for(int j = 0; j<*numberOfPipedCommands; j++) {
-			//createWholesomeThreadFor(commandString);
-			printf("%s\n",commands[j]);
+			int* commandArgc=malloc(sizeof *commandArgc);
+			char** commandArgv = splitByCharacter(removeEdgeSpaces(commands[j]), commandArgc, ' ');
+			
+			for(int k = 0; k < *commandArgc; k++) {
+				printf("	|%s|\n", commandArgv[k]);
+				//the above printf prints arguments (the same format as argv[]) of a command.
+				//so "cd -d -xd" will print "cd", "-d" "-xd".
+			}
+		
+			if(j==*numberOfPipedCommands-1) { //if this is the last cycle of this for loop
+				free(commandArgc);
+				free(commandArgv);
+			}
+			printf("\n");
+
 		}
 		
 		free(commands);
+		free(numberOfPipedCommands);
 	}
 	
 	else {  //for normal commands (without pipe)
 		printf("%s\n", commandString);
 		/*createWholesomeFork(commandString)*/
-	}; 
+	};
 }
 
 int main(int argc, char* argv[]) {
-	int argsBad = checkArgs(argc, argv);
-	if(argsBad) return argsBad;
+	int argsOK = checkArgs(argc, argv);
+	if(argsOK) return argsOK;
 		
 	const char* pathToTaskfile = argv[1];
 	const char* pathToOutfile = argv[2];
@@ -35,5 +49,7 @@ int main(int argc, char* argv[]) {
 	for(int i = 0; i < *numberOfTasks; i++) {
 		handleCommand(tasks[i].command);
 	}
+
+
 	return 0;
 }
