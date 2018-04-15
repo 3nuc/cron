c8a8ca9694376fa27cb60a36c338ac60e1976d60
artur <artur@t430.t430>
2018-04-15 19:11:05 +0300

    Add generujtest.sh script for testing
    This bash script will create a crontaskfile.test file with 6 tasks inside,
    3 of them to be executed at the current time, 3 of them in the next minute.
    (eg if you run it at 15:35, it will create 3 tasks for 15:35 and 3 for 15:36).
    
    The tasks created in the taskfile are unsorted - the minicron program should
    sort them on its own.

---
+++ src/generujtest.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+hourminute=$(date --date="+1 minute" +%H:%M:) 
+second=$(date +%S)
+colon=:
+
+set -e
+
+make -s
+rm -f crontaskfile.test
+
+declare -a arr=("date +%y/%m"
+		"date +%y/%m/%d | echo pipe!"
+		"uptime --pretty | uname -r | echo more pipes")
+
+if [ "$second" -gt "57" ]; then
+	printf 'Sleeping 4 seconds...\\n'
+	sleep 4
+fi
+
+for i in "${arr[@]}"
+do
+	myrand=$(($RANDOM % 3))
+	echo "$hourminute$i$colon$myrand$newline" >> crontaskfile.test
+done
+
+./minicron crontaskfile.test outfile.txt
